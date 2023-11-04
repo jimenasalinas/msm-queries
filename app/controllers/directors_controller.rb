ce1ec4cec8c7f53c4a825fccbc26512e90b6c8c1
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-11-04 06:02:22 +0000

    finished eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,6 +12,12 @@ class DirectorsController < ApplicationController
     render(template: "director_templates/list_young")
   end
 
+  def index_eldest
+    matching_young = Director.where.not(dob: nil)
+    eldest_director = matching_young.order(dob: :asc).first
+    @eldest_director = eldest_director
+    render(template: "director_templates/list_eldest")
+  end
 
   def show
     the_id = params.fetch("the_id")
